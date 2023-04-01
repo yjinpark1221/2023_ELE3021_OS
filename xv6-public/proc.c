fbca7f6b8a304dc646288ecc2d002a2e57feebf0
ppulppyo@hanyang.ac.kr <2020002960@hanyang.ac.kr>
2023-04-01 19:34:54 +0900

    move wrapper function to sysproc.c, add wiki

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -538,47 +538,6 @@ procdump(void)
 
 // project1 scheduler
 
-// Wrapper for system calls
-int sys_yield(void)
-{
-  yield();
-  return 0;
-}
-
-int sys_getLevel(void)
-{
-  return getLevel();
-}
-
-int sys_setPriority(void)
-{
-  int arguments[2];
-  if (argint(2, arguments) < 0)
-    return -1;
-  if (arguments[0] < 0 || arguments[1] < 0)
-    return -1;
-  setPriority(arguments[0], arguments[1]);
-  return 0;
-}
-
-int sys_schedulerLock(void)
-{
-  int password;
-  if (argint(1, &password) < 0)
-    return -1;
-  schedulerLock(password);
-  return 0;
-}
-
-int sys_schedulerUnlock(void)
-{
-  int password;
-  if (argint(1, &password) < 0)
-    return -1;
-  schedulerUnlock(password);
-  return 0;
-}
-
 int getLevel()
 {
   return myproc()->queueLevel;
