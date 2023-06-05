61fda542163ff011e0d1a1a0b69db920e20f6fac
ppulppyo@hanyang.ac.kr <2020002960@hanyang.ac.kr>
2023-06-05 09:15:31 +0900

    add Multi indirect and Symbolic link

---
+++ project3_filesystem/ln.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 4){
+    printf(2, "Usage: ln [-h / -s] old new\n");
+    exit();
+  }
+  if (strcmp(argv[1], "-h") == 0) {
+    if (link(argv[2], argv[3]) < 0) {
+      printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+    }
+  }
+  else if (strcmp(argv[1], "-s") == 0) {
+    if (symlink(argv[2], argv[3]) < 0) {
+      printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+    }
+  }
+  exit();
+}
